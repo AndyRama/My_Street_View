bb4aca15cbfb85ec9ebf1d6c0347ddc893065147
Alexis Roger <a.roger37@gmail.com>
2020-12-01 09:54:55 +0100

    merged master

--- app/models/question.rb
+++ app/models/question.rb
@@ -1,10 +1,6 @@
 class Question < ApplicationRecord
   belongs_to :project
-<<<<<<< HEAD
   has_many :feedbacks
-=======
-  has_many :feedbacks, dependent: :destroy
->>>>>>> 8d3387d7c2a61acc3ac7ff2e6ecec5c9293f10bd
 
   validates :title, presence: true
   validates :first_answer, presence: true
